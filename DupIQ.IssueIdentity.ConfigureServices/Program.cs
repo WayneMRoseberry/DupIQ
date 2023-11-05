afea619a74ba099d5dfe2c98ffb54ee293cdde33
WayneRoseberryGit <wayne_roseberry@hotmail.com>
2023-11-04 18:33:42 -0700

    Added configuration command line tools

---
+++ DupIQ.IssueIdentity.ConfigureServices/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DupIQ.IssueIdentityProviders.Sql;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine($"{System.AppDomain.CurrentDomain.FriendlyName}");
+
+var builder = new ConfigurationBuilder().AddUserSecrets<Program>();
+var thing = builder.Build();
+var SqlConfig = thing.GetSection("SqlIOHelperConfig").Get<SqlIOHelperConfig>();
+
+SqlIssueDbIOHelper sqlIOHelper = new SqlIssueDbIOHelper(SqlConfig);
+SqlTenantDatabaseHelper sqlTenantDatabaseHelper = new SqlTenantDatabaseHelper(SqlConfig);
+sqlTenantDatabaseHelper.ConfigureTenantDatabase();
+sqlIOHelper.ConfigureDatabaseServer();
+SqlUserDbHelper sqlUserDbHelper = new SqlUserDbHelper(SqlConfig);
+sqlUserDbHelper.ConfigureDatabase();
---
+++ DupIQ.IssueIdentity.ConfigureServices/readme.md
@@ -0,0 +1,12 @@
+﻿User secrets are stored in the following format:
+
+```
+{
+  "SqlIOHelperConfig": {
+    "ConnectionString": "",
+    "ServerName": "<servername>",
+    "DatabaseName": "<databasename>",
+    "AccountName": "<sql server account>",
+    "Password": "<password to sql account>"
+}
+```
