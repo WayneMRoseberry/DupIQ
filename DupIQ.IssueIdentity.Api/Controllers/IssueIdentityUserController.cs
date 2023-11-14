f0a75484a7a6b414cf931434c5190af871c2ae22
WayneRoseberryGit <wayne_roseberry@hotmail.com>
2023-11-14 08:56:25 -0800

    Configuring the JWTBearer authentication, adding Authorize button to Swagger

--- DupIQ.IssueIdentity.Api/Controllers/IssueIdentityUserController.cs
+++ DupIQ.IssueIdentity.Api/Controllers/IssueIdentityUserController.cs
@@ -1,5 +1,8 @@
 ﻿using DupIQ.IssueIdentity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace DupIQ.IssueIdentityAPI.Controllers
 {
@@ -33,6 +36,27 @@ namespace DupIQ.IssueIdentityAPI.Controllers
 		public void DeleteUser(string userId)
 		{
 			GlobalConfiguration.UserManager.DeleteUser(userId);
+		}
+
+		[Microsoft.AspNetCore.Mvc.HttpPost("/token")]
+		public string GetToken(IssueIdentityUser user)
+		{
+			var issuer = _configuration["Jwt:Issuer"];
+			var audience = _configuration["Jwt:Audience"];
+			var expiry = DateTime.Now.AddMinutes(120);
+			var securityKey = new SymmetricSecurityKey
+		(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+			var credentials = new SigningCredentials
+		(securityKey, SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(issuer: issuer,
+		audience: audience,
+		expires: expiry,
+		signingCredentials: credentials);
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var stringToken = tokenHandler.WriteToken(token);
+			return stringToken;
 		}
 	}
 }
--- DupIQ.IssueIdentity.Api/Program.cs
+++ DupIQ.IssueIdentity.Api/Program.cs
@@ -1,15 +1,71 @@
 using DupIQ.IssueIdentity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthentication(o =>
+{
+	o.DefaultAuthenticateScheme = JwtBearerDefaults.
+AuthenticationScheme;
+	o.DefaultChallengeScheme = JwtBearerDefaults.
+AuthenticationScheme;
+	o.DefaultScheme = JwtBearerDefaults.
+AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+	o.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = false,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["Jwt:Issuer"],
+		ValidAudience = builder.Configuration["Jwt:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey
+(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+	};
+});
 builder.Services.AddAuthorization();
 
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+var securityScheme = new OpenApiSecurityScheme()
+{
+	Name = "Authorization",
+	Type = SecuritySchemeType.ApiKey,
+	Scheme = "Bearer",
+	BearerFormat = "JWT",
+	In = ParameterLocation.Header,
+	Description = "JSON Web Token based security",
+};
+
+var securityReq = new OpenApiSecurityRequirement()
+				{
+					{
+					 new OpenApiSecurityScheme
+					 {
+					 Reference = new OpenApiReference
+					 {
+					 Type = ReferenceType.SecurityScheme,
+					 Id = "Bearer"
+					 }
+					 },
+					 new string[] {}
+					}
+				};
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+	o.AddSecurityDefinition("Bearer", securityScheme);
+	o.AddSecurityRequirement(securityReq);
+});
 
 var app = builder.Build();
 
